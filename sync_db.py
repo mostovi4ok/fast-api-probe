9e09d2fd876bb27863e66d53949370e91b4a5e56
Петр Плотников <plotnikov.petr1993@gmail.com>
2023-01-21 17:34:55 +0700

    add click

--- sync_db.py
+++ sync_db.py
@@ -1,15 +1,25 @@
-import sys
+from typing import Literal
+
+import click
 
 from fast.models import Base
 from fast.db import engine
 
 
-def main(args: list[str]) -> None:
-    if args == ["create"] or not args:
+@click.command()
+@click.option(
+    "--db_action",
+    default="create",
+    type=click.Choice(["create", "drop"]),
+)
+def main(db_action: Literal["create", "drop"]) -> None:
+    """Program for creating or deleting a database."""
+
+    if db_action == "create":
         Base.metadata.create_all(engine)
-    elif args == ["drop"]:
+    elif db_action == "drop":
         Base.metadata.drop_all(engine)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
